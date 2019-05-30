9223a72baba7e41c1dd89e4fc3d6e7de0879945f
Guilherme Almeida <guilherme.almeida@ivoryit.com.br>
2019-05-30 15:35:17 -0300

    Implementação de reports sobre os testes automatizados.

--- src/components/news/__tests__/NewsList.test.js
+++ src/components/news/__tests__/NewsList.test.js
@@ -4,7 +4,7 @@ import { render, mount } from '../../../setupTests';
 import NewsList from '../NewsList';
 
 describe('<NewsList />', () => {
-  it('Deve possuir um titulo # g1', () => {
+  it('[UNIT] - Deve possuir um titulo # g1', () => {
     //arrange
     const hasSearchBar = true
     const component = render(<NewsList debug  searchbar={hasSearchBar}/>);
@@ -14,16 +14,16 @@ describe('<NewsList />', () => {
     expect(header[0]['children'][0].data).toEqual('# g1')
     expect(component).toMatchSnapshot();
   });
-  it('Ao pesquisar por wsj deve possuir um titulo # wsj', () => {
+  it('[INTEGRATION]- Ao pesquisar por wsj deve possuir um titulo # wsj', () => {
       //arrange
       const hasSearchBar = true
       const component = mount(<NewsList debug  searchbar={hasSearchBar}/>);
       let busca =  component.find('.filterItens input')
       //act
       // Mudar o estado da aplicacao
-      busca.prop('onChange')( {target: {value: 'wsj'}} )
+      busca.simulate('change', {target: {value: 'wsj'}} )
       //Renderizar a aplicacoe novamente e selecionar o campo a ser verificado
-      component.update()
+      //component.update()
       let header = component.find('.newssourceHeader h1')
       //assert
       expect(header.at(0).text()).toEqual('# wsj')
